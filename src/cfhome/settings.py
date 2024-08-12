28d47b4182fa789bf49681dd3d1d8917a46a62b1
RAHUL BISWAL <72703616+Rahul-Biswal-new@users.noreply.github.com>
2024-08-12 08:49:03 -0700

    features/added django debug

--- src/cfhome/settings.py
+++ src/cfhome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2o@n)j%$i$p(*#ex$-m0f)$-l+9^9*3x)ic1k!32m6(6(sftvv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+
+# print("DEBUG",DEBUG)
 
 ALLOWED_HOSTS = [".railway.app"]
 
