837da4f5106a8a5024d9196b14c66883bc1a6652
RAHUL BISWAL <72703616+Rahul-Biswal-new@users.noreply.github.com>
2024-08-14 13:38:39 -0700

    bugfix/debug-fix

--- src/cfhome/settings.py
+++ src/cfhome/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2o@n)j%$i$p(*#ex$-m0f)$-l+9^9*3x)ic1k!32m6(6(sftvv'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
