b581dd1af614b74eb7df3c09d0dcc0781b71b1f5
RAHUL BISWAL <72703616+Rahul-Biswal-new@users.noreply.github.com>
2024-09-08 13:15:38 -0400

    features/env

--- src/projecthome/settings.py
+++ src/projecthome/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-0xjt-%%b8i%$)a$to!#svsb^zc=544759398a&&6^1rx%dlb80
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app'
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ] 
 
 
 # Application definition
